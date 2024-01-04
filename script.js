b235610fa9e2606c143b589db71c1601aec77aa1
rhlsingh19 <145037560+rhlsingh19@users.noreply.github.com>
2024-01-04 21:05:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-function firstChar(text) {
-  // your code here
+function firstChar(inputString) {
+    if (!inputString || inputString.trim() === '') {
+        return '';
+    }
+
+    for (let i = 0; i < inputString.length; i++) {
+        if (inputString[i] !== ' ') {
+            return inputString[i];
+        }
+    }
+
+    return '';
 }
 
-// Do not change the code below
+// Examples
+console.log(firstChar(' Rosa Parks ')); // Output: 'R'
+console.log(firstChar(' Hello World ')); // Output: 'H'
+console.log(firstChar(' ')); // Output: ''
+console.log(firstChar('')); // Output: ''
 
 const text = prompt("Enter text:");
 alert(firstChar(text));
